d9a93eb7ffe7c0f369aeb2c52204214519169595
Ваше Имя <_email@example.com>
2025-02-01 16:16:27 +0500

    go

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://thingproxy.freeboard.io/fetch/https://api.football-data.org/v4'
+export const baseUrl = 'https://api.football-data.org/v4'
 export const API_KEY = '17fc0364ad1b420185138d0f628b81b5';
